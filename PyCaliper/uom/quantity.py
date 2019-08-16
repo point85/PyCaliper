eb9b690e6a532a4628d190128afde87b8b134dd7
billran <kentra@pacbell.net>
2019-08-16 15:35:44 -0700

    wip

--- PyCaliper/uom/quantity.py
+++ PyCaliper/uom/quantity.py
@@ -4,6 +4,7 @@ from PyCaliper.uom.prefix import Prefix
 from PyCaliper.uom.unit import Unit
 from PyCaliper.uom.measurement_system import MeasurementSystem
 from builtins import staticmethod
+import math
 
 class Quantity(Symbolic):
     def __init__(self, amount: float, uom: UnitOfMeasure):
@@ -25,6 +26,9 @@ class Quantity(Symbolic):
             if (self.amount == other.amount and self.uom == other.uom):
                 answer = True
             return answer
+        
+    def __str__(self):
+        return self.amount + ", [" + str(self.uom) + "] " + str(super)
    
     @staticmethod
     def createAmount(self, value) -> float:
@@ -67,6 +71,20 @@ class Quantity(Symbolic):
 
         # create the quantity now
         return Quantity(newAmount, toUOM)
+    
+    def convertToUnit(self, unit: Unit) -> Quantity:
+        return self.convert(MeasurementSystem.instance().getUOM(unit))
+    
+    def convertToPrefixUnit(self, prefix: Prefix, unit: Unit) -> Quantity:
+        return self.convert(MeasurementSystem.instance().getUOM(prefix, unit))
+    
+    def convertToPowerProduct(self, uom1: UnitOfMeasure, uom2: UnitOfMeasure) -> Quantity:
+        newUOM = self.uom.clonePowerProduct(uom1, uom2)
+        return self.convert(newUOM)
+    
+    def convertToPower(self, uom: UnitOfMeasure) -> Quantity:
+        newUOM = self.uom.clonePower(uom)
+        return self.convert(newUOM)
         
     def subtract(self, other: Quantity) -> Quantity:
         toSubtract = other.convert(self.uom)
@@ -78,6 +96,53 @@ class Quantity(Symbolic):
         amount = self.amount + toAdd.amount
         return Quantity(amount, self.uom)
     
+    def divide(self, other: Quantity) -> Quantity:
+        if (other.amount == 0.0):
+            msg = MeasurementSystem.getMessage("divisor.cannot.be.zero")
+            raise Exception(msg)
+
+        amount = self.amount / other.amount
+        newUOM = self.uom.divide(other.uom)
+        return Quantity(amount, newUOM)
+    
+    def divideByAmount(self, divisor: float) -> Quantity:
+        return  Quantity(self.amount / divisor, self.uom)
+    
+    def multiply(self, other: Quantity) -> Quantity:
+        amount = self.amount * other.amount
+        newUOM = self.uom.multiply(other.uom)
+        return Quantity(amount, newUOM)
+    
+    def multiplyByAmount(self, multiplier: float) -> Quantity:
+        return Quantity(self.amount * multiplier, self.uom)
+    
+    def power(self, exponent: int) -> Quantity:
+        amount = math.pow(self.amount, exponent)
+        newUOM = MeasurementSystem.instance().createPowerUOM(self.uom, exponent)
+        return Quantity(amount, newUOM) 
+    
+    def invert(self) -> Quantity:
+        amount = 1.0 / self.amount
+        uom = self.uom.invert()
+        return Quantity(amount, uom)  
+    
+    def compare(self, other: Quantity) -> int:
+        toCompare = other
+        
+        if (self.uom != other.uom):
+            # first try converting the units
+            toCompare = other.convert(self.uom)
+        
+        if (math.isclose(self.amount, toCompare.amount)):
+            return 0
+        elif (self.amount < toCompare.amount):
+            return -1
+        else:
+            return 1
+
+    def classify(self) -> Quantity:
+        self.uom.classify()
+        return self
 """
 
 """    
