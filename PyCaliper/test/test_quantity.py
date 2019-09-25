fca3180ea33a486754dcf94015d20bc146a343b1
billran <kentra@pacbell.net>
2019-09-25 14:18:33 -0700

    wip

--- PyCaliper/test/test_quantity.py
+++ PyCaliper/test/test_quantity.py
@@ -1106,3 +1106,35 @@ class TestQuantity(unittest.TestCase):
         one1 = Quantity(1.0, msys.getOne())
         q1 = one1.convertToPower(one)
         self.assertAlmostEqual(q1.amount, 1.0, None, None, TestUtils.DELTA6)
+
+    def testEnergy(self):
+        msys = MeasurementSystem.instance()
+        
+        # A nutrition label says the energy content is 1718 KJ.  What is this amount in kilo-calories?
+        kJ = msys.createPrefixedUOM(Prefix.kilo(), msys.getUOM(Unit.JOULE))
+        kcal = msys.createPrefixedUOM(Prefix.kilo(), msys.getUOM(Unit.CALORIE))
+        kcalQ = Quantity(1718.0, kJ).convert(kcal)
+        self.assertAlmostEqual(kcalQ.amount, 410.6, None, None, TestUtils.DELTA1)
+        
+        # A Tesla Model S battery has a capacity of 100 KwH.  
+        # When fully charged, how many electrons are in the battery?
+        c = msys.getQuantity(Constant.LIGHT_VELOCITY)
+        me = msys.getQuantity(Constant.ELECTRON_MASS)   
+        kwh = msys.createPrefixedUOM(Prefix.kilo(), msys.getUOM(Unit.WATT_HOUR))
+        kwhQ = Quantity(100.0, kwh)
+        
+        wh = kwhQ.convert(msys.getUOM(Unit.WATT_HOUR))
+        self.assertTrue(wh.amount == 1.0E+05)
+        
+        electrons = kwhQ.divide(c).divide(c).divide(me)
+        d = electrons.amount / 1.221E12
+        self.assertAlmostEqual(d, 1.0, None, None, TestUtils.DELTA1)
+   
+    def testClassification(self):
+        msys = MeasurementSystem.instance()
+        
+        mass = Quantity(1035.0, msys.getUOM(Unit.KILOGRAM)) 
+        volume = Quantity(1000.0, msys.getUOM(Unit.LITRE)) 
+        density = mass.divide(volume).classify()
+        self.assertTrue(density.uom.unitType == UnitType.DENSITY)
+
--- PyCaliper/test/test_snippet.py
+++ PyCaliper/test/test_snippet.py
@@ -5,32 +5,30 @@ from PyCaliper.uom.cache_manager import CacheManager
 from PyCaliper.uom.enums import Unit, UnitType, Constant
 from PyCaliper.uom.quantity import Quantity
 from PyCaliper.test.test_utils import TestUtils
+from PyCaliper.uom.prefix import Prefix
 
 class TestPrefix(unittest.TestCase):
     def testSnippet(self):
         msys = MeasurementSystem.instance()
         
-        one = msys.getOne()
-        mps = msys.getUOM(Unit.METRE_PER_SEC)
-        fps = msys.getUOM(Unit.FEET_PER_SEC)
-        nm = msys.getUOM(Unit.NEWTON_METRE)
-        ft = msys.getUOM(Unit.FOOT)
-        inch = msys.getUOM(Unit.INCH)
-        mi = msys.getUOM(Unit.MILE)
-        hr = msys.getUOM(Unit.HOUR)
-        m = msys.getUOM(Unit.METRE)
-        s = msys.getUOM(Unit.SECOND)
-        n = msys.getUOM(Unit.NEWTON)
-        lbf = msys.getUOM(Unit.POUND_FORCE)
-        m2 = msys.getUOM(Unit.SQUARE_METRE)
-        m3 = msys.getUOM(Unit.CUBIC_METRE)
-        ft2 = msys.getUOM(Unit.SQUARE_FOOT)
-
-        # test products and quotients
-        #ftlbf = msys.getUOM(Unit.FOOT_POUND_FORCE)
-        #CacheManager.instance().unregisterUOM(ftlbf)
-        nmQ = Quantity(1.0, nm)
-        lbfinQ = nmQ.convertToPowerProduct(lbf, inch)
-        self.assertAlmostEqual(lbfinQ.amount, 8.850745791327183, None, None, TestUtils.DELTA6)
+        # A nutrition label says the energy content is 1718 KJ.  What is this amount in kilo-calories?
+        kJ = msys.createPrefixedUOM(Prefix.kilo(), msys.getUOM(Unit.JOULE))
+        kcal = msys.createPrefixedUOM(Prefix.kilo(), msys.getUOM(Unit.CALORIE))
+        kcalQ = Quantity(1718.0, kJ).convert(kcal)
+        self.assertAlmostEqual(kcalQ.amount, 410.6, None, None, TestUtils.DELTA1)
+        
+        # A Tesla Model S battery has a capacity of 100 KwH.  
+        # When fully charged, how many electrons are in the battery?
+        c = msys.getQuantity(Constant.LIGHT_VELOCITY)
+        me = msys.getQuantity(Constant.ELECTRON_MASS)   
+        kwh = msys.createPrefixedUOM(Prefix.kilo(), msys.getUOM(Unit.WATT_HOUR))
+        kwhQ = Quantity(100.0, kwh)
+        
+        wh = kwhQ.convert(msys.getUOM(Unit.WATT_HOUR))
+        self.assertTrue(wh.amount == 1.0E+05)
+        
+        electrons = kwhQ.divide(c).divide(c).divide(me)
+        d = electrons.amount / 1.221E12
+        self.assertAlmostEqual(d, 1.0, None, None, TestUtils.DELTA1)
         print("Done")
         
